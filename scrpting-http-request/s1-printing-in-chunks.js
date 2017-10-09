3fbf6cff70ffe660852cbdc560afa468ee6c9b28
Amir <amir.ghandeharioon@gmail.com>
2017-10-09 16:47:55 +0000

    step 1: done

---
+++ scrpting-http-request/s1-printing-in-chunks.js
@@ -0,0 +1,17 @@
+var https = require('https');
+
+var requestOptions = {
+  host : 'sytantris.github.io',
+  path : '/http-examples/step1.html'
+};
+
+function getAndPrintHTML(requestOptions){
+  https.get(requestOptions,function(response){
+   response.setEncoding('utf8');
+   response.on('data', function(data){
+    console.log('\n', data);
+   });
+  });
+}
+
+getAndPrintHTML(requestOptions);
